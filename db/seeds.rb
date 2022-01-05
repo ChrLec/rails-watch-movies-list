d9129a5207a0d64aa205a92ba6285a203f9d1047
Christelle Lecamp <christelle.lecamp@gmail.com>
2022-01-05 11:38:29 +0100

    seed with api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies ..."
+for i in 1..8
+  api_url = "http://tmdb.lewagon.com/movie/popular?&page=#{i}"
+  URI.open(api_url) do |stream|
+    films = JSON.parse(stream.read)["results"]
+    films.each do |hash|
+      movie = Movie.create!(title: hash["original_title"], overview: hash["overview"], poster_url: "https://image.tmdb.org/t/p/original#{hash["poster_path"]}", rating: hash["vote_average"])
+      puts "Created #{movie.title}"
+    end
+  end
+end
+puts "Finished!"
